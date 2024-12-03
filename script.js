ce501877c2ac0317f292cda0bdc74c677a98f34b
roman alekseev <roma.alekseev.90@bk.ru>
2024-12-03 10:32:20 +0300

    Добавлена возможность закрытия по кнопке Esc и по нажатию на подложку

--- script.js
+++ script.js
@@ -90,4 +90,16 @@ document.addEventListener("DOMContentLoaded", () => {
   closeModalBtn.addEventListener("click", () => {
     modal.style.display = "none";
   });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape" && modal.style.display === "flex") {
+      modal.style.display = "none";
+    }
+  });
 });
